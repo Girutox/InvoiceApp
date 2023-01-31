9d0cce28c3571d7bc8530519016840ece7e82c1c
Giancarlo Illescas <giancarlo.hoyos@gmail.com>
2023-01-31 14:18:35 -0500

    add font folder and filas and link to styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import './styles/globals.scss'
-import styles from './App.module.scss';
+import "./App.scss";
+import "./fonts/Spartan-Medium.ttf";
+import "./fonts/Spartan-Bold.ttf";
 
-import BasicHeader from './components/organisms/basicHeader';
+import BasicHeader from "./components/organisms/basicHeader";
 
 function App() {
   return (
-    <div className={styles.appContainer}>
+    <div className="app-container">
       <BasicHeader />
-      <main>
-
-      </main>
+      <main></main>
     </div>
   );
 }
