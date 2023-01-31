d1b1187b082ff8cdaeebe0fd7e3d85439c6aeee7
Giancarlo Illescas <giancarlo.hoyos@gmail.com>
2023-01-31 11:58:32 -0500

    clear default markup of app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <header>
+      </header>
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
