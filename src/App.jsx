9b674b1226dccf003d246c68c45a3af8934df711
Giancarlo Illescas <giancarlo.hoyos@gmail.com>
2023-01-31 12:24:58 -0500

    change to css modules in App default files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css';
+import styles from './App.module.css';
+
+import BasicHeader from './components/organisms/basicHeader';
 
 function App() {
   return (
-    <div className="app-container">
-      <header>
-      </header>
+    <div className={styles.appContainer}>
+      <BasicHeader />
       <main>
 
       </main>
