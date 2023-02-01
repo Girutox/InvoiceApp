cd3a4927c0b813ffb0bf8a76dcdc90393e848fa6
Giancarlo Illescas <giancarlo.hoyos@gmail.com>
2023-02-01 14:38:16 -0500

    fix style of texts

--- src/components/atoms/typography/typographyDescriptor.jsx
+++ src/components/atoms/typography/typographyDescriptor.jsx
@@ -2,7 +2,7 @@ import styles from './typographyDescriptor.module.scss';
 
 const TypographyDescriptor = (props) => {
   return (
-    <h4 className={styles.heading}>{props.children}</h4>
+    <h4 className={`${styles.heading} body2`}>{props.children}</h4>
   );
 }
  
