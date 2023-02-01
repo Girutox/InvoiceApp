c894b1d3c1c240060c2a8c2ca5dce08c27250078
Giancarlo Illescas <giancarlo.hoyos@gmail.com>
2023-01-31 23:57:33 -0500

    add model folder and first file

---
+++ src/models/designSystemColors.js
@@ -0,0 +1,242 @@
+export const mainData = [
+  {
+    id: 1,
+    classNameLabel1: "color-white",
+    backgroundColor: "bgc-primary",
+    hexColorText: "#7C5DFA",
+    subData: [
+      {
+        id: 1,
+        classNameLabel2: "color-secondary-2",
+        colorType: "RGB",
+        colorValue: "124, 93, 250",
+      },
+      {
+        id: 2,
+        classNameLabel2: "color-secondary-2",
+        colorType: "HSL",
+        colorValue: "252°, 94%, 67%",
+      },
+    ],
+  },
+  {
+    id: 2,
+    classNameLabel1: "color-white",
+    backgroundColor: "bgc-primary-2",
+    hexColorText: "#9277FF",
+    subData: [
+      {
+        id: 1,
+        classNameLabel2: "color-secondary-2",
+        colorType: "RGB",
+        colorValue: "146, 119, 255",
+      },
+      {
+        id: 2,
+        classNameLabel2: "color-secondary-2",
+        colorType: "HSL",
+        colorValue: "252°, 100%, 73%",
+      },
+    ],
+  },
+  {
+    id: 3,
+    classNameLabel1: "color-white",
+    backgroundColor: "bgc-primary-3",
+    hexColorText: "#1E2139",
+    subData: [
+      {
+        id: 1,
+        classNameLabel2: "color-secondary-2",
+        colorType: "RGB",
+        colorValue: "30, 33, 57",
+      },
+      {
+        id: 2,
+        classNameLabel2: "color-secondary-2",
+        colorType: "HSL",
+        colorValue: "233°, 31%, 17%",
+      },
+    ],
+  },
+  {
+    id: 4,
+    classNameLabel1: "color-white",
+    backgroundColor: "bgc-primary-4",
+    hexColorText: "#252945",
+    subData: [
+      {
+        id: 1,
+        classNameLabel2: "color-secondary-2",
+        colorType: "RGB",
+        colorValue: "37, 41, 69",
+      },
+      {
+        id: 2,
+        classNameLabel2: "color-secondary-2",
+        colorType: "HSL",
+        colorValue: "233°, 30%, 21%",
+      },
+    ],
+  },
+  {
+    id: 5,
+    classNameLabel1: "color-secondary-3",
+    backgroundColor: "bgc-secondary",
+    hexColorText: "#DFE3FA",
+    subData: [
+      {
+        id: 1,
+        classNameLabel2: "color-secondary-2",
+        colorType: "RGB",
+        colorValue: "223, 227, 250",
+      },
+      {
+        id: 2,
+        classNameLabel2: "color-secondary-2",
+        colorType: "HSL",
+        colorValue: "231°, 73%, 93%",
+      },
+    ],
+  },
+  {
+    id: 6,
+    classNameLabel1: "color-white",
+    backgroundColor: "bgc-secondary-2",
+    hexColorText: "#888EB0",
+    subData: [
+      {
+        id: 1,
+        classNameLabel2: "color-secondary-2",
+        colorType: "RGB",
+        colorValue: "136, 142, 176",
+      },
+      {
+        id: 2,
+        classNameLabel2: "color-secondary-2",
+        colorType: "HSL",
+        colorValue: "231°, 20%, 61%",
+      },
+    ],
+  },
+  {
+    id: 7,
+    classNameLabel1: "color-white",
+    backgroundColor: "bgc-secondary-3",
+    hexColorText: "#7E88C3",
+    subData: [
+      {
+        id: 1,
+        classNameLabel2: "color-secondary-2",
+        colorType: "RGB",
+        colorValue: "126, 136, 195",
+      },
+      {
+        id: 2,
+        classNameLabel2: "color-secondary-2",
+        colorType: "HSL",
+        colorValue: "231°, 37%, 63%",
+      },
+    ],
+  },
+  {
+    id: 8,
+    classNameLabel1: "color-white",
+    backgroundColor: "bgc-secondary-4",
+    hexColorText: "#0C0E16",
+    subData: [
+      {
+        id: 1,
+        classNameLabel2: "color-secondary-2",
+        colorType: "RGB",
+        colorValue: "12, 14, 22",
+      },
+      {
+        id: 2,
+        classNameLabel2: "color-secondary-2",
+        colorType: "HSL",
+        colorValue: "228°, 29%, 7%",
+      },
+    ],
+  },
+  {
+    id: 9,
+    classNameLabel1: "color-white",
+    backgroundColor: "bgc-alert",
+    hexColorText: "#EC5757",
+    subData: [
+      {
+        id: 1,
+        classNameLabel2: "color-secondary-2",
+        colorType: "RGB",
+        colorValue: "236, 87, 87",
+      },
+      {
+        id: 2,
+        classNameLabel2: "color-secondary-2",
+        colorType: "HSL",
+        colorValue: "0°, 80%, 63%",
+      },
+    ],
+  },
+  {
+    id: 10,
+    classNameLabel1: "color-white",
+    backgroundColor: "bgc-alert-2",
+    hexColorText: "#9277FF",
+    subData: [
+      {
+        id: 1,
+        classNameLabel2: "color-secondary-2",
+        colorType: "RGB",
+        colorValue: "255, 151, 151",
+      },
+      {
+        id: 2,
+        classNameLabel2: "color-secondary-2",
+        colorType: "HSL",
+        colorValue: "0°, 100%, 80%",
+      },
+    ],
+  },
+  {
+    id: 11,
+    classNameLabel1: "color-secondary-2",
+    backgroundColor: "bgc-background-light",
+    hexColorText: "#F8F8FB (Light BG)",
+    subData: [
+      {
+        id: 1,
+        classNameLabel2: "color-secondary-2",
+        colorType: "RGB",
+        colorValue: "248, 248, 251",
+      },
+      {
+        id: 2,
+        classNameLabel2: "color-secondary-2",
+        colorType: "HSL",
+        colorValue: "240°, 27%, 98%",
+      },
+    ],
+  },
+  {
+    id: 12,
+    classNameLabel1: "color-white",
+    backgroundColor: "bgc-alert-4",
+    hexColorText: "#141625",
+    subData: [
+      {
+        id: 1,
+        classNameLabel2: "color-secondary-2",
+        colorType: "RGB",
+        colorValue: "20, 22, 37",
+      },
+      {
+        id: 2,
+        classNameLabel2: "color-secondary-2",
+        colorType: "HSL",
+        colorValue: "233°, 30%, 11%",
+      },
+    ],
+  },
+];
