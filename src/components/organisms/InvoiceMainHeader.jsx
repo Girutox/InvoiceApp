91730f8fdd0a0f82f7d5661428043b692fc378d0
Giancarlo Illescas <giancarlo.hoyos@gmail.com>
2023-03-07 11:12:02 -0500

    add custom hook for windows size

--- src/components/organisms/InvoiceMainHeader.jsx
+++ src/components/organisms/InvoiceMainHeader.jsx
@@ -1,24 +1,42 @@
+import PropTypes from 'prop-types';
+
+import useWindowDimensions from '../../hooks/useWindowDimensions';
+
 import SpanCustom from '../atoms/spanCustom';
 import TitleSubtitle from '../molecules/TitleSubtitle';
 import Button1Default from '../organisms/buttons/button1Default';
 
 import styles from './InvoiceMainHeader.module.scss';
 
-const InvoiceMainHeader = (props) => {
+const InvoiceMainHeader = ({ mainData }) => {
+  const { width } = useWindowDimensions();
+
+  let subTitle = `There are ${mainData.length} total invoices`;
+  let filterTerxt = 'Filter by status';
+  let newButtonText = 'New Invoice';
+  if (width <= 375) {
+    subTitle = `${mainData.length} total invoices`;
+    filterTerxt = 'Filter';
+    newButtonText = 'New';
+  }
+
   return (
     <div className={styles.container}>
       <TitleSubtitle
         className={styles.titleSubTitleContainer}
         title="Invoices"
-        subTitle={`There are ${props.mainData.length} total invoices`}
+        subTitle={subTitle}
       />
       <div className={styles.filterGroup}>
-        <SpanCustom className="body2 color-secondary-4 text-bold">Filter by status</SpanCustom>
+        <SpanCustom className="body2 color-secondary-4 text-bold">{filterTerxt}</SpanCustom>
         <SpanCustom className="body2 color-primary text-bold">&#32;</SpanCustom>
       </div>
-      <Button1Default spanText="New Invoice" />
+      <Button1Default spanText={newButtonText} />
     </div>
   );
+};
+InvoiceMainHeader.propTypes = {
+  mainData: PropTypes.array
 };
 
 export default InvoiceMainHeader;
